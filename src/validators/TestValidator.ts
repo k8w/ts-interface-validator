d3b18370a7aaa8e9ed2c7b6c8b1abdae0f69d5d3
wzy <twoeo@qq.com>
2017-08-31 18:14:27 +0800

    use nyc

--- src/validators/TestValidator.ts
+++
@@ -1,8 +0,0 @@
-import IValidator from "./IValidator";
-import ValidateResult from "../models/ValidateResult";
-export default class TestValidator implements IValidator{
-    validate(value:any):ValidateResult{
-        console.log('TestValidator', value);
-        return ValidateResult.success;
-    }
-}
